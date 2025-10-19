522a4e2d654614adc5c905f70dbb3ef3d0e14c28
dropz <bondarcukpavel4@gmail.com>
2025-10-19 21:32:50 +1000

    Починил баг, из-за которого бекенд не мог подключиться к бд

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -4,11 +4,11 @@ import {TypeOrmModuleOptions} from "@nestjs/typeorm";
 export async function getTypeOrmConfig(configService: ConfigService): Promise<TypeOrmModuleOptions> {
     return {
         type: 'postgres',
-        host: configService.getOrThrow<string>('DB_HOST'),
-        port: configService.getOrThrow<number>('DB_PORT'),
-        username: configService.getOrThrow<string>('DB_USER'),
-        password: configService.getOrThrow<string>('POSTGRES_PASSWORD') || 'user',
-        database: configService.getOrThrow<string>('DB_NAME'),
+        host: configService.getOrThrow<string>('POSTGRES_HOST'),
+        port: configService.getOrThrow<number>('POSTGRES_PORT'),
+        username: configService.getOrThrow<string>('POSTGRES_USER'),
+        password: configService.getOrThrow<string>('POSTGRES_PASSWORD'),
+        database: configService.getOrThrow<string>('POSTGRES_DB'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: true,
         autoLoadEntities: true,
